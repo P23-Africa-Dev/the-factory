f6fec618fda3cb09f687e894bdd8e17740994dfd
Elijah Obominuru <elijahobominuru@gmail.com>
2026-04-01 14:19:51 +0100

    feat: implement login form component and integrate into auth page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,15 @@
+import LoginForm from "@/components/forms/login-form";
+
 export default function LoginPage() {
-  return <div>Login</div>;
+  return (
+    <div className="w-full max-w-[460px] flex flex-col gap-8">
+      <div className="text-left md:text-center flex flex-col gap-3">
+        <h2 className="text-[32px] font-extrabold leading-10 tracking-[0px] text-gray-900 mb-2.5">
+          Login to Continue
+        </h2>
+      </div>
+
+      <LoginForm />
+    </div>
+  );
 }
